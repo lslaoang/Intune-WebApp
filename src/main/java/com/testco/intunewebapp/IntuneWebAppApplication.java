c40cdd1ad5ab3bb1f1340ccc3a9656a107427c6e
Laurel Lao Ang <laurellaoang@gmail.com>
2022-03-28 21:55:51 +0800

    Initial commit

---
+++ src/main/java/com/testco/intunewebapp/IntuneWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.testco.intunewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntuneWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntuneWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testco/intunewebapp/IntuneWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testco.intunewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntuneWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
