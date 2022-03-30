b29085d82f4a5c7ccfeebf99b9c3c24c0547da16
Laurel Lao Ang <laurellaoang@gmail.com>
2022-03-30 20:02:11 +0800

    Actuator added

--- src/main/java/com/testco/intunewebapp/config/AADOAuth2ResourceServerSecurityConfig.java
+++ src/main/java/com/testco/intunewebapp/config/AADOAuth2ResourceServerSecurityConfig.java
@@ -9,17 +9,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AADOAuth2ResourceServerSecurityConfig extends AADResourceServerWebSecurityConfigurerAdapter {
-    /**
-     * Add configuration logic as needed.
-     */
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         // @formatter:off
         http
                 .authorizeRequests(auth -> auth
-                        .antMatchers(HttpMethod.GET, "/api/v1/check").permitAll()
-                        .antMatchers(HttpMethod.GET, "/api/v1/verify").hasAuthority("SCOPE_Consumer.read")
+                        .mvcMatchers("/health").permitAll()
+                        .mvcMatchers(HttpMethod.GET, "/api/v1/check").authenticated()
+                        .mvcMatchers(HttpMethod.GET, "/api/v1/verify").hasAuthority("SCOPE_Consumer.read")
                         .anyRequest().authenticated());
         // @formatter:on
     }
--- src/main/java/com/testco/intunewebapp/controller/IntuneController.java
+++ src/main/java/com/testco/intunewebapp/controller/IntuneController.java
@@ -7,19 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServlet;
-
 @RestController
 @RequestMapping("${api.endpoints.base-path}")
 public class IntuneController {
 
-    @GetMapping(path ="/verify")
-    public ResponseEntity<?> verify(){
-        return new ResponseEntity<>( new Accepted(), HttpStatus.ACCEPTED);
+    @GetMapping(path = "/verify")
+    public ResponseEntity<?> verify() {
+        return new ResponseEntity<>(new Accepted(), HttpStatus.ACCEPTED);
     }
 
     @GetMapping(path = "/check")
-    public ResponseEntity<?> check(){
-        return new ResponseEntity<>( "All Good!", HttpStatus.ACCEPTED);
+    public ResponseEntity<?> check() {
+        return new ResponseEntity<>("All Good!", HttpStatus.ACCEPTED);
     }
 }
