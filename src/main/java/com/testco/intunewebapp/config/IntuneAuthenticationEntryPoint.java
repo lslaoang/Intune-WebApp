3d242e782d9816393bec9d6779185255e3648c3c
Laurel Lao Ang <laurellaoang@gmail.com>
2022-03-31 08:47:37 +0800

    Filter updated

--- src/main/java/com/testco/intunewebapp/config/IntuneAuthenticationEntryPoint.java
+++ src/main/java/com/testco/intunewebapp/config/IntuneAuthenticationEntryPoint.java
@@ -1,6 +1,6 @@
 package com.testco.intunewebapp.config;
 
-import com.testco.intunewebapp.model.response.UnAuthorizedAccess;
+import com.testco.intunewebapp.model.response.AccessDenied;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
@@ -25,7 +25,7 @@ public class IntuneAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        writeErrorResponse(response, new UnAuthorizedAccess());
+        writeErrorResponse(response, new AccessDenied());
         LOGGER.severe(String.format("Access from %s denied.", request.getRemoteAddr()));
 
     }
