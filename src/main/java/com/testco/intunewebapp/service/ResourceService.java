46b9da20558386dba782dfef4098277347a10a39
Laurel Lao Ang <laurellaoang@gmail.com>
2022-04-28 11:41:55 +0800

    css added

--- src/main/java/com/testco/intunewebapp/service/ResourceService.java
+++ src/main/java/com/testco/intunewebapp/service/ResourceService.java
@@ -2,13 +2,12 @@ package com.testco.intunewebapp.service;
 
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.net.URI;
 
-import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
 @Service
 public class ResourceService {
@@ -27,13 +26,13 @@ public class ResourceService {
         this.webClient = webClient;
     }
 
-    public void checkResource(OAuth2AuthorizedClient authorizedClient) {
+    public void checkResource() {
         LOGGER.info("Checking of resources started.");
         try {
             webClient
                     .get()
                     .uri(new URI(resourceBaseUri + resourceEndpoint))
-                    .attributes(oauth2AuthorizedClient(authorizedClient))
+                    .attributes(clientRegistrationId("testco-res"))
                     .retrieve()
                     .bodyToMono(String.class)
                     .block();
