7d5bf9951afd213b71519f2c38a3545f7c5cf662
Laurel Lao Ang <laurellaoang@gmail.com>
2022-06-27 16:28:43 +0800

    update controller

--- src/main/java/com/testco/intunewebapp/controller/IntuneController.java
+++ src/main/java/com/testco/intunewebapp/controller/IntuneController.java
@@ -1,20 +1,20 @@
 package com.testco.intunewebapp.controller;
 
-import com.testco.intunewebapp.model.response.Accepted;
-import com.testco.intunewebapp.model.response.UnAuthorizedAccess;
 import com.testco.intunewebapp.service.ResourceService;
 import com.testco.intunewebapp.service.VerifyService;
+import com.testco.iw.api.VerifyApi;
+import com.testco.iw.models.Accepted;
+import com.testco.iw.models.Forbidden;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("${api.base-path}")
-public class IntuneController {
+public class IntuneController implements VerifyApi {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(IntuneController.class);
 
@@ -26,14 +26,14 @@ public class IntuneController {
         this.resourceService = resourceService;
     }
 
-    @GetMapping(path = "/verify")
-    public ResponseEntity<?> verify() {
+    @Override
+    public ResponseEntity<Accepted> verify() {
         try {
             resourceService.checkResource();
             verifyService.authorize();
         } catch (RuntimeException e) {
             LOGGER.warn("Authorization check failed. {}", e.getMessage());
-            return new ResponseEntity<>(new UnAuthorizedAccess(), HttpStatus.FORBIDDEN);
+            return new ResponseEntity(new Forbidden(), HttpStatus.FORBIDDEN);
         }
         return new ResponseEntity<>(new Accepted(), HttpStatus.ACCEPTED);
     }
