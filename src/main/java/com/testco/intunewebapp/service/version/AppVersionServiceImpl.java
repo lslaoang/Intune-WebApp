05e69a881fb89cf936ee6fe04db1b302ffeae9e9
Laurel Lao Ang <laurellaoang@gmail.com>
2022-07-03 14:49:45 +0800

    update version checker

--- src/main/java/com/testco/intunewebapp/service/version/AppVersionServiceImpl.java
+++ src/main/java/com/testco/intunewebapp/service/version/AppVersionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.testco.intunewebapp.service.version;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,16 +11,30 @@ public class AppVersionServiceImpl implements AppVersionService {
 
     private static final Logger LOGGER = Logger.getLogger(AppVersionService.class.getName());
 
-    private final String APP_VERSION_HEADER = "X-UBS-APP-VERSION";
-    private final String APP_VERSION_VALID = "1.2.0";
+    protected final String APP_VERSION_HEADER = "X-UBS-APP-VERSION";
+    protected final String APP_OS_HEADER = "X-UBS-APP-OS";
+
+    @Value("{app.version.ios}")
+    private String APP_VERSION_IOS;
+
+    @Value("{app.version.android}")
+    private String APP_VERSION_ANDROID;
 
     @Override
     public boolean isCorrectVersion(HttpServletRequest request) {
-        String appVersion;
+        String appVersion, appOS;
         try {
+            appOS = request.getHeader(APP_OS_HEADER);
             appVersion = request.getHeader(APP_VERSION_HEADER);
-            if (!appVersion.equals(APP_VERSION_VALID)) {
-                return false;
+            if(appOS.equalsIgnoreCase("ios")){
+                if (!appVersion.equals(APP_VERSION_IOS)) {
+                    return false;
+                }
+            }
+            if(appOS.equalsIgnoreCase("android")){
+                if (!appVersion.equals(APP_VERSION_ANDROID)) {
+                    return false;
+                }
             }
         } catch (NullPointerException e){
             LOGGER.severe("App version not detected.");
