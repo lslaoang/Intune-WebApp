53bb394f54f49b7be03bc41e67743ab9dd1310c2
Laurel Lao Ang <laurellaoang@gmail.com>
2022-07-04 18:13:31 +0800

    add os checker

--- src/main/java/com/testco/intunewebapp/service/version/AppVersionServiceImpl.java
+++ src/main/java/com/testco/intunewebapp/service/version/AppVersionServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @Service
@@ -14,30 +16,32 @@ public class AppVersionServiceImpl implements AppVersionService {
     protected final String APP_VERSION_HEADER = "X-UBS-APP-VERSION";
     protected final String APP_OS_HEADER = "X-UBS-APP-OS";
 
-    @Value("{app.version.ios}")
+    @Value("${app.version.ios}")
     private String APP_VERSION_IOS;
 
-    @Value("{app.version.android}")
+    @Value("${app.version.android}")
     private String APP_VERSION_ANDROID;
 
     @Override
     public boolean isCorrectVersion(HttpServletRequest request) {
-        String appVersion, appOS;
+
+        LOGGER.info("Checking application version...");
+        Map<String, String> appVersionMap = new HashMap<>();
+        appVersionMap.put("ios", APP_VERSION_IOS);
+        appVersionMap.put("android", APP_VERSION_ANDROID);
+
+        String requestVersion, requestOs;
         try {
-            appOS = request.getHeader(APP_OS_HEADER);
-            appVersion = request.getHeader(APP_VERSION_HEADER);
-            if(appOS.equalsIgnoreCase("ios")){
-                if (!appVersion.equals(APP_VERSION_IOS)) {
-                    return false;
-                }
+            requestOs = request.getHeader(APP_OS_HEADER);
+            requestVersion = request.getHeader(APP_VERSION_HEADER);
+
+            String appVersion = appVersionMap.get(requestOs.toLowerCase());
+            if (!appVersion.equals(requestVersion)) {
+                LOGGER.warning(String.format("Invalid application version. Application should be %s version.", appVersion));
+                return false;
             }
-            if(appOS.equalsIgnoreCase("android")){
-                if (!appVersion.equals(APP_VERSION_ANDROID)) {
-                    return false;
-                }
-            }
-        } catch (NullPointerException e){
-            LOGGER.severe("App version not detected.");
+        } catch (NullPointerException e) {
+            LOGGER.severe("Application version or operating system not detected." + e.getMessage());
             return false;
         }
         LOGGER.info("Valid application version.");
