f9ad9d06b04221dc6bea9e9627f71c6cdd325837
Laurel Lao Ang <laurellaoang@gmail.com>
2022-07-14 14:06:29 +0800

    add minimum version compatibility

--- src/main/java/com/testco/intunewebapp/service/version/VersionBodyServiceImpl.java
+++ src/main/java/com/testco/intunewebapp/service/version/VersionBodyServiceImpl.java
@@ -19,23 +19,31 @@ public class VersionBodyServiceImpl implements VersionBodyService{
     private String APP_VERSION_ANDROID;
 
     @Override
-    public void verifyVersion(String appOs, String appVersion) {
+    public void verifyVersion(String reqAppOs, String reqAppVersion) {
 
         try{
             LOGGER.info("Checking application version...");
-            Map<String, String> appVersionMap = new HashMap<>();
-            appVersionMap.put("ios", APP_VERSION_IOS);
-            appVersionMap.put("android", APP_VERSION_ANDROID);
 
-            if(!appVersion.equals(appVersionMap.get(appOs))){
+            String okVersionAndroid = majorVersion(APP_VERSION_ANDROID);
+            String okVersionIos = majorVersion(APP_VERSION_IOS);
+
+            Map<String, String> appVersionMap = new HashMap<>();
+            appVersionMap.put("ios", okVersionIos);
+            appVersionMap.put("android", okVersionAndroid);
+
+            if(!appVersionMap.get(reqAppOs).equalsIgnoreCase(reqAppVersion)){
                 LOGGER.severe("Version not compatible");
                 throw new VersionException("Version not accepted.");
             }
 
         }catch (RuntimeException e){
-            LOGGER.severe("Error occurred while checking version.");
+            LOGGER.severe("Error occurred while checking version." + e);
             throw new VersionException("Something went wrong.");
         }
+        LOGGER.info("Version compatible.");
+    }
 
+    public String majorVersion(String version){
+        return version.length() > 2 ? version : version.substring(0,2);
     }
 }
