346ffd949861f9c06a143f8e8038ce74d1435082
Laurel Lao Ang <laurellaoang@gmail.com>
2022-08-03 10:30:30 +0800

    add properties to models

--- src/main/java/com/testco/intunewebapp/model/FileUploadRequest.java
+++ src/main/java/com/testco/intunewebapp/model/FileUploadRequest.java
@@ -1,4 +1,13 @@
 package com.testco.intunewebapp.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class FileUploadRequest {
+    private UploadFile file;
+    private UploadMetadata metadata;
 }
--- src/main/java/com/testco/intunewebapp/model/UploadMetadata.java
+++ src/main/java/com/testco/intunewebapp/model/UploadMetadata.java
@@ -1,4 +1,17 @@
 package com.testco.intunewebapp.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class UploadMetadata {
+    private String scanCountry;
+    private boolean copyOfOriginal;
+    private String businessDomain;
+    private String businessDocument;
+    private String businessRelationId;
+    private boolean directProcessing;
 }
