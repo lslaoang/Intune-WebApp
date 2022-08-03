bdb13ddba72e781783dfee3e3fff31e4cdefc71f
Laurel Lao Ang <laurellaoang@gmail.com>
2022-08-03 10:36:52 +0800

    add inner class

--- src/main/java/com/testco/intunewebapp/model/UploadMetadata.java
+++ src/main/java/com/testco/intunewebapp/model/UploadMetadata.java
@@ -4,14 +4,31 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Builder
 public class UploadMetadata {
     private String scanCountry;
     private boolean copyOfOriginal;
-    private String businessDomain;
-    private String businessDocument;
-    private String businessRelationId;
+    private DocumentDomain documentDomain;
+    private FileDestination fileDestination;
     private boolean directProcessing;
+
+    @Getter
+    @Setter
+    @Builder
+    private static class DocumentDomain {
+        private String businessDomain;
+        private String businessDocument;
+        private String businessRelationId;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    private static class FileDestination {
+        private List<String> fileDestinationInbox;
+    }
 }
