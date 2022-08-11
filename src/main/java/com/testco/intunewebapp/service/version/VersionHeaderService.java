f1425b2898e5d625230b8f7eb386c7b07ca31829
Laurel Lao Ang <laurellaoang@gmail.com>
2022-08-11 19:16:50 +0800

    remove @deprecated

--- src/main/java/com/testco/intunewebapp/service/version/VersionHeaderService.java
+++ src/main/java/com/testco/intunewebapp/service/version/VersionHeaderService.java
@@ -3,7 +3,7 @@ package com.testco.intunewebapp.service.version;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Deprecated
+
 public interface VersionHeaderService {
     void verifyVersion(HttpServletRequest request);
 }
