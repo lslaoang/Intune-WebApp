4d056b50123c77b26bc4c9441e533adf4001f62e
Laurel Lao Ang <laurellaoang@gmail.com>
2022-11-03 19:16:30 +0800

    update exception catching

--- src/main/java/com/testco/intunewebapp/handler/IntuneAuthenticationEntryPoint.java
+++ src/main/java/com/testco/intunewebapp/handler/IntuneAuthenticationEntryPoint.java
@@ -3,9 +3,12 @@ package com.testco.intunewebapp.handler;
 import com.testco.iw.models.AccessDenied;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +20,12 @@ import static com.testco.intunewebapp.util.ResponseWriterUtil.writeErrorResponse
 public class IntuneAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private static final Logger LOGGER = Logger.getLogger(IntuneAuthenticationEntryPoint.class.getName());
+
+    @ExceptionHandler(OAuth2AuthenticationException.class)
+    public ResponseEntity<Object> handleInvalidAuth(){
+        LOGGER.severe("Failed to authenticate request.");
+        return new ResponseEntity<>(new AccessDenied(), HttpStatus.UNAUTHORIZED);
+    }
 
     @Override
     public void commence(HttpServletRequest request,
