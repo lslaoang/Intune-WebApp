c1acf8ec982ed64d0cd4fac4b91b75b8f1abc429
Laurel Lao Ang <laurellaoang@gmail.com>
2022-11-08 20:12:20 +0800

    add ResponseCode

---
+++ src/main/java/com/testco/intunewebapp/constant/ResponseCode.java
@@ -0,0 +1,14 @@
+package com.testco.intunewebapp.constant;
+
+public class ResponseCode {
+
+    public final static Integer BAD_REQUEST=4000;
+    public final static Integer BAD_REQUEST=4000;
+    public final static Integer BAD_REQUEST=4000;
+    public final static Integer BAD_REQUEST=4000;
+    public final static Integer BAD_REQUEST=4000;
+    public final static Integer BAD_REQUEST=4000;  
+    public final static Integer BAD_REQUEST=4000;
+    public final static Integer BAD_REQUEST=4000;
+
+}
