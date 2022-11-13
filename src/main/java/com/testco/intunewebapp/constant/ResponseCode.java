092530120de2d367058a12e77ff3176b54d90c1f
Laurel Lao Ang <laurellaoang@gmail.com>
2022-11-13 12:00:12 +0800

    update error code

--- src/main/java/com/testco/intunewebapp/constant/ResponseCode.java
+++ src/main/java/com/testco/intunewebapp/constant/ResponseCode.java
@@ -3,12 +3,12 @@ package com.testco.intunewebapp.constant;
 public class ResponseCode {
 
     public final static Integer BAD_REQUEST=4000;
-    public final static Integer BAD_REQUEST=4000;
-    public final static Integer BAD_REQUEST=4000;
-    public final static Integer BAD_REQUEST=4000;
-    public final static Integer BAD_REQUEST=4000;
-    public final static Integer BAD_REQUEST=4000;  
-    public final static Integer BAD_REQUEST=4000;
-    public final static Integer BAD_REQUEST=4000;
+    public final static Integer ACCESS_DENIED=4010;
+    public final static Integer TOKEN_INVALID=4011;
+    public final static Integer TOKEN_EXPIRED=4012;
+    public final static Integer FORBIDDEN_ACCESS=4030;
+    public final static Integer INTERNAL_ERROR=5000;
+    public final static Integer CLIENT_ERROR_400=5001;
+    public final static Integer CLIENT_ERROR_500=5002;
 
 }
