61c2202e33a59c5b8998f99caa72d10480b1defe
Laurel Lao Ang <laurellaoang@gmail.com>
2022-11-13 12:05:32 +0800

    api update

--- src/main/java/com/testco/intunewebapp/constant/ResponseCode.java
+++ src/main/java/com/testco/intunewebapp/constant/ResponseCode.java
@@ -6,6 +6,7 @@ public class ResponseCode {
     public final static Integer ACCESS_DENIED=4010;
     public final static Integer TOKEN_INVALID=4011;
     public final static Integer TOKEN_EXPIRED=4012;
+    public final static Integer UPDATE_REQUIRED=4020;
     public final static Integer FORBIDDEN_ACCESS=4030;
     public final static Integer INTERNAL_ERROR=5000;
     public final static Integer CLIENT_ERROR_400=5001;
